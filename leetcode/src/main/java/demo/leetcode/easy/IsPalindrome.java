7bd04e39713b4891821d093478279611fbef7016
sunqinwen <sunqinwen@bilibili.com>
2019-02-27 19:30:07 +0800

    threadlocal

--- leetcode/src/main/java/demo/leetcode/easy/IsPalindrome.java
+++ leetcode/src/main/java/demo/leetcode/easy/IsPalindrome.java
@@ -38,6 +38,7 @@ public class IsPalindrome {
         System.out.println(isPalindrome.isPalindromeV2(1225221));
     }
 
+    // O(n)
     private boolean isPalindrome(int x) {
         if (x < 0) {
             return false;
@@ -59,12 +60,9 @@ public class IsPalindrome {
 
     }
 
+    // O(n/2)
     private boolean isPalindromeV2(int x) {
-        // 特殊情况：
-        // 如上所述，当 x < 0 时，x 不是回文数。
-        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
-        // 则其第一位数字也应该是 0
-        // 只有 0 满足这一属性
+
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
