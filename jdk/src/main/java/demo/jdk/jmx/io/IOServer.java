7938df014c0b29c38eb0e573177693e7b57c2f16
sunqinwen <sunqinwen@bilibili.com>
2019-03-12 09:37:14 +0800

    class loader test

--- jdk/src/main/java/demo/jdk/jmx/io/IOServer.java
+++ jdk/src/main/java/demo/jdk/jmx/io/IOServer.java
@@ -13,43 +13,39 @@ public class IOServer {
 
     public static void main(String[] args) throws IOException {
 
+        ServerSocket serverSocket = null;
+        int recvMsgSize = 0;
+        InputStream in = null;
         try {
-            ServerSocket serverSocket = new ServerSocket(2333);
+            serverSocket = new ServerSocket(2333);
             byte[] recvBuf = new byte[1024];
             while (true) {
-
-                int recvMsgSize = 0;
-                InputStream in = null;
-                try {
-                    Socket clntSocket = serverSocket.accept();
-                    SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
-                    System.out.println("连接成功，处理客户端：" + clientAddress);
-                    in = clntSocket.getInputStream();
-                    while ((recvMsgSize = in.read(recvBuf)) != -1) {
-                        byte[] temp = new byte[recvMsgSize];
-                        System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
-                        System.out.println("收到客户端" + clientAddress + "的消息内容：" + new String(temp));
-                    }
-                    System.out.println("-------------------------");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (serverSocket != null) {
-                            System.out.println("socket关闭！");
-                            serverSocket.close();
-                        }
-                        if (in != null) {
-                            System.out.println("stream连接关闭！");
-                            in.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                Socket clntSocket = serverSocket.accept();
+                SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
+                System.out.println("连接成功，处理客户端：" + clientAddress);
+                in = clntSocket.getInputStream();
+                while ((recvMsgSize = in.read(recvBuf)) != -1) {
+                    byte[] temp = new byte[recvMsgSize];
+                    System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
+                    System.out.println("收到客户端" + clientAddress + "的消息内容：" + new String(temp));
                 }
+                System.out.println("-------------------------");
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    System.out.println("socket关闭！");
+                    serverSocket.close();
+                }
+                if (in != null) {
+                    System.out.println("stream连接关闭！");
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
