ab4d91c66899fed7d98b1dbdeea0430d037e08cd
sunqinwen <sunqinwen@bilibili.com>
2019-03-12 19:59:06 +0800

    NIO

--- jdk/src/main/java/demo/jdk/jmx/io/IOServer.java
+++ jdk/src/main/java/demo/jdk/jmx/io/IOServer.java
@@ -5,31 +5,45 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Create by 18073 on 2019/3/12.
  */
 public class IOServer {
 
+    private static ExecutorService ctp = Executors.newCachedThreadPool();
+
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = null;
-        int recvMsgSize = 0;
-        InputStream in = null;
+
         try {
-            serverSocket = new ServerSocket(2333);
+            serverSocket = new ServerSocket(2333); //开一个监听2333端口的TCP服务
             byte[] recvBuf = new byte[1024];
             while (true) {
-                Socket clntSocket = serverSocket.accept();
-                SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
+                Socket clntSocket = serverSocket.accept(); //探听有没有新的客户端连接进来，没有就阻塞
+                SocketAddress clientAddress = clntSocket.getRemoteSocketAddress(); //通过跟服务连接上的客户端socket，拿到客户端地址
                 System.out.println("连接成功，处理客户端：" + clientAddress);
-                in = clntSocket.getInputStream();
-                while ((recvMsgSize = in.read(recvBuf)) != -1) {
-                    byte[] temp = new byte[recvMsgSize];
-                    System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
-                    System.out.println("收到客户端" + clientAddress + "的消息内容：" + new String(temp));
-                }
-                System.out.println("-------------------------");
+
+
+                ctp.execute(() -> {
+                    int recvMsgSize = 0;
+                    InputStream in = null; //数据流
+                    try {
+                        in = clntSocket.getInputStream();
+                        while ((recvMsgSize = in.read(recvBuf)) != -1) { //读取发送的数据，当客户端未断开连接，且不往服务端发数据的时候，说明一直处于准备读的状态，会一直阻塞下去，直到有数据写入（读就绪）
+                            byte[] temp = new byte[recvMsgSize];
+                            System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
+                            System.out.println("收到客户端" + clientAddress + "的消息内容：" + new String(temp)); //打印消息
+                        }
+                        System.out.println("-----------------------------------");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,10 +52,6 @@ public class IOServer {
                 if (serverSocket != null) {
                     System.out.println("socket关闭！");
                     serverSocket.close();
-                }
-                if (in != null) {
-                    System.out.println("stream连接关闭！");
-                    in.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
--- jdk/src/main/java/demo/jdk/jmx/io/IOTest.java
+++ jdk/src/main/java/demo/jdk/jmx/io/IOTest.java
@@ -13,32 +13,36 @@ import java.util.concurrent.TimeUnit;
  */
 public class IOTest {
 
-    public static ExecutorService ctp = Executors.newCachedThreadPool();
+    private static ExecutorService ctp = Executors.newCachedThreadPool();
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            ctp.submit(IOTest::client);
+            ctp.submit(IOTest::client); //并发十个客户端连接过去
         }
     }
 
     public static void client() {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer buffer = ByteBuffer.allocate(1024); //定义缓冲区
         SocketChannel socketChannel = null;
         try {
-            socketChannel = SocketChannel.open();
-            socketChannel.configureBlocking(false);
-            socketChannel.connect(new InetSocketAddress("127.0.0.1", 2333));
-            if (socketChannel.finishConnect()) {
-                int i = 0;
-                while (i < 5) {
-                    TimeUnit.SECONDS.sleep(10);
-                    String info = "来自客户端的第" + (i++) + "条消息";
-                    buffer.clear();
-                    buffer.put(info.getBytes());
-                    buffer.flip();
-                    while (buffer.hasRemaining()) {
-                        socketChannel.write(buffer);
+            socketChannel = SocketChannel.open(); //打开SocketChannel
+            socketChannel.configureBlocking(false); //设置为非阻塞模式
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 2333)); //连接服务
+            while (true) {
+                if(socketChannel.finishConnect()){ //这里的finishConnect是尝试连接，有可能返回false，因此使用死循环进行连接检查，确保连接已经正常建立。
+                    System.out.println("客户端已连接到服务器");
+                    int i = 0;
+                    while (i < 5) {
+                        TimeUnit.SECONDS.sleep(1); //隔一秒钟写一条
+                        String info = "来自客户端的第" + (i++) + "条消息";
+                        buffer.clear();
+                        buffer.put(info.getBytes());
+                        buffer.flip();
+                        while (buffer.hasRemaining()) {
+                            socketChannel.write(buffer); //给服务发消息
+                        }
                     }
+                    break;
                 }
             }
         } catch (IOException | InterruptedException e) {
@@ -46,6 +50,7 @@ public class IOTest {
         } finally {
             try {
                 if (socketChannel != null) {
+                    System.out.println("客户端Channel关闭");
                     socketChannel.close();
                 }
             } catch (IOException e) {
