8e26174cce22be8900f566912f14f7abc116cf6c
sunqinwen <sunqinwen@bilibili.com>
2019-04-23 19:47:41 +0800

    UUID

--- jdk/src/main/java/demo/jdk/unsafe/UnsafePlayer.java
+++ jdk/src/main/java/demo/jdk/unsafe/UnsafePlayer.java
@@ -14,14 +14,14 @@ import java.lang.reflect.Field;
  */
 public class UnsafePlayer {
     public static void main(String[] args) throws Exception {
-        //通过反射实例化Unsafe
+        /*//通过反射实例化Unsafe
         Field f = Unsafe.class.getDeclaredField("theUnsafe");
         f.setAccessible(true);
         Unsafe unsafe = (Unsafe) f.get(null);
         //实例化Player
         Player player = (Player) unsafe.allocateInstance(Player.class);
         player.setName("li lei");
-        System.out.println(player.getName());
+        System.out.println(player.getName());*/
     }
 }
 
