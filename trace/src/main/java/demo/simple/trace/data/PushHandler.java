ae556c3e60ad78bb03a9a206b7473c3e412e572f
sunqinwen <sunqinwen@bilibili.com>
2019-05-13 19:09:04 +0800

    del useless code

--- trace/src/main/java/demo/simple/trace/data/PushHandler.java
+++ trace/src/main/java/demo/simple/trace/data/PushHandler.java
@@ -5,8 +5,6 @@
 
 package demo.simple.trace.data;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import demo.simple.trace.core.SimpleSpan;
 
 import java.util.concurrent.BlockingQueue;
