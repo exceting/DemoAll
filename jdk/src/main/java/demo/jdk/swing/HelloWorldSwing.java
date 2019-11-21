eb8c0eb365a227adbaf2c64c2d3f9c61ccc176fb
sunqinwen <sunqinwen@bilibili.com>
2019-11-22 00:46:20 +0800

    引入swing

--- jdk/src/main/java/demo/jdk/swing/HelloWorldSwing.java
+++ jdk/src/main/java/demo/jdk/swing/HelloWorldSwing.java
@@ -1,7 +1,6 @@
 package demo.jdk.swing;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Create by 18073 on 2019/11/20.
@@ -9,14 +8,12 @@ import java.awt.*;
 public class HelloWorldSwing {
 
     public static void main(String[] args) {
-        JFrame jf = new JFrame("Java第二个GUI程序");    //创建一个JFrame对象
-        jf.setBounds(300, 100, 400, 200);    //设置窗口大小和位置
-        JPanel jp = new JPanel();    //创建一个JPanel对象
-        JLabel jl = new JLabel("这是放在JPanel上的标签");    //创建一个标签
-        jp.setBackground(Color.white);    //设置背景色
-        jp.add(jl);    //将标签添加到面板
-        jf.add(jp);    //将面板添加到窗口
-        jf.setVisible(true);    //设置窗口可见
+        JFrame jf = new JFrame("Pleiades -singularity"); //创建一个JFrame对象
+        jf.setSize(1500, 800);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // 设置窗体关闭方式
+        jf.setLocationRelativeTo(null);
+        jf.setIconImage(new ImageIcon("./bilibili.png").getImage());
+        jf.setVisible(true);  // 设置窗体可视
     }
 
 }
