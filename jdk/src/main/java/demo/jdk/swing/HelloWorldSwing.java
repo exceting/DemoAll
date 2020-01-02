3ef63ded56cac3849b8b12e9d3520c9c0ad1d40e
sunqinwen <sunqinwen@bilibili.com>
2020-01-03 05:55:21 +0800

    引入swing

--- jdk/src/main/java/demo/jdk/swing/HelloWorldSwing.java
+++ jdk/src/main/java/demo/jdk/swing/HelloWorldSwing.java
@@ -1,6 +1,7 @@
 package demo.jdk.swing;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Create by 18073 on 2019/11/20.
@@ -8,12 +9,27 @@ import javax.swing.*;
 public class HelloWorldSwing {
 
     public static void main(String[] args) {
-        JFrame jf = new JFrame("Pleiades -singularity"); //创建一个JFrame对象
+        JFrame jf = new JFrame("Pleiades Singularity"); //创建一个JFrame对象
         jf.setSize(1500, 800);
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // 设置窗体关闭方式
         jf.setLocationRelativeTo(null);
-        jf.setIconImage(new ImageIcon("./bilibili.png").getImage());
+        jf.setIconImage(new ImageIcon("bilibili.png").getImage());
         jf.setVisible(true);  // 设置窗体可视
+
+        Container c = jf.getContentPane();
+
+        JPanel jp = new JPanel();
+        jp.setBackground(new Color(54, 54, 54));
+        JLabel title = new JLabel("欢迎使用Singularity");
+        jp.add(title);
+
+        JLabel title2 = new JLabel("万物之始");
+        jp.add(title2);
+
+        JLabel image = new JLabel(new ImageIcon("bilibili.png"));
+        jp.add(image);
+
+        c.add(jp);
     }
 
 }
